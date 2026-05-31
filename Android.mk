c796061fb33631cd1b20573412067edb77db838b
scamdisk <158341159+scamdisk@users.noreply.github.com>
2026-05-31 18:59:48 +0300

    add wifi channel to android.mk

--- Android.mk
+++ Android.mk
@@ -236,6 +236,7 @@ LOCAL_SRC_FILES += \
 	telephony/java/com/android/internal/telephony/ISms.aidl \
 	telephony/java/com/android/internal/telephony/ITelephonyRegistry.aidl \
 	telephony/java/com/android/internal/telephony/IWapPushManager.aidl \
+        wifi/java/android/net/wifi/WifiChannel.aidl \
 	wifi/java/android/net/wifi/IWifiManager.aidl \
 	wifi/java/android/net/wifi/p2p/IWifiP2pManager.aidl
 #
